3609935b9ab51c9804db93dc6fff69a6bb3e674b
98-f355-f1 <thomas.branson@inbox.lv>
2023-10-01 08:53:30 +0300

    Last fix

--- cli.js
+++ cli.js
@@ -62,7 +62,7 @@ const transformSassFilesToEsModules = (directoryToSearch, pattern) => {
 
       const fileContent = fs.readFileSync(subDirectoryToSearch).toString();
       const data = config.prepare(fileContent);
-      const result = fs.renderSync({ data, }).css.toString();
+      const result = renderSync({ data, }).css.toString();
 
       console.log(`fileContent: ${fileContent}`);
 
